dfaf1094b6d474a4b95de90525f4685603de9b26
Ярослав <106529918+Yarri4@users.noreply.github.com>
2023-03-24 12:12:21 +0300

    Валидация на заполнение данных

--- scripts/index.js
+++ scripts/index.js
@@ -59,3 +59,46 @@ shadow.addEventListener("click", function (e) {
     onShadowForForm();
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+  }
+
+  function formValidate(e) {
+    let error = 0;
+    let formReq = document.querySelectorAll(".__req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.classList.contains("_phone")) {
+        if (phoneTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+  }
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function phoneTest(input) {
+    return !/^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/.test(input.value);
+  }
+});
